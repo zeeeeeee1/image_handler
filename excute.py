bfc72b3bed6c91c8b0699a9f4fa7772cfd029539
Ryosei Takahashi <zexel8716@gmail.com>
2020-08-11 08:47:22 +0900

    余計なprintは削除。画像サイズのcsv表記を、実際の画像のサイズ表記と合わせる。

--- excute.py
+++ excute.py
@@ -11,7 +11,6 @@ import imghdr
 
 ssl._create_default_https_context = ssl._create_unverified_context
 def get_images(keyword, num):
-    print("https://www.google.com/search?hl=jp&q=" + keyword + "&btnG=Google+Search&tbs=0&safe=off&tbm=isch", "url")
     Res = requests.get("https://www.google.com/search?hl=jp&q=" + keyword + "&btnG=Google+Search&tbs=0&safe=off&tbm=isch")
     Html = Res.text
     Soup = bs4.BeautifulSoup(Html,'lxml')
@@ -34,12 +33,11 @@ def download_img(url, imgpath):
 def put_converted_img(img, converted_imgpath):
     converted_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     converted_img = cv2.rotate(converted_img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-    # print(converted_img, "converted_img")
     cv2.imwrite(converted_imgpath, converted_img)
 
 def get_size(img):
     height, width = img.shape[:2]
-    return str(height) + "×" + str(width)
+    return str(width) + "×" + str(height)
 
 def put_csv(data):
     with open('data.csv', 'w') as file:
@@ -58,16 +56,16 @@ csv_rows.append(["ファイル名", "サイズ", "フォーマット"])
 for i,image in enumerate(images):
     filename = keyword + "_" + str(i)
     imgpath = "./img/" + filename + ".png"
+    # 1秒間待つ
+    time.sleep(1)
     if download_img(image["src"], imgpath) == True:
         ext = imghdr.what(imgpath)
-        print(ext, "ext")
         img = cv2.imread(imgpath)
         # csvの行情報を追加。ファイル名,サイズ,フォーマット
         csv_rows.append([imgpath, get_size(img), ext])
         put_converted_img(img, "./img_converted/" + filename + "." + ext)
-        time.sleep(1)
 
 put_csv(csv_rows)
 
 
-print("OK")
+print("画像の保存、変換が完了しました。")
